2177f74e0f000ac37f6bca6ed9ffa981407769fa
mehdielmoussaoui1 <elmoussaoui.mehdi7@gmail.com>
2023-08-24 03:03:55 -0700

    simple_shel

---
+++ ero.h
@@ -0,0 +1,3 @@
+char *message_selector(general_t info);
+void error(general_t *info);
+void error_extra(general_t *info, char *extra);
---
+++ memo.h
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+
+
+/* memory.c */
+void *_realloc(void *ptr, size_t old_size, size_t new_size);
+
+/* free.c */
+void free_memory_p(void *ptr);
+void free_memory_pp(void **ptr);
